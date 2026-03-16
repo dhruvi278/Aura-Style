24a559817f40283d36eac29bde023f7aa7b2d7db
AyushMahera <ayush.mahera@aliansoftware.com>
2026-03-16 09:52:14 +0530

    Resolved errors and app.jsx conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,17 @@ import Formfield from './components/Formfield'
 import Navbar from './components/Navbar'
 
 import TotalItems from './components/wardrobe/TotalItems'
-<<<<<<< HEAD
 import HomePage from './pages/HomePage'
-=======
-import GenerateOutfit from './pages/GenerateOutfit'
->>>>>>> 2c0948850b490aac93efeddf521e8484fe14e9a6
 import Wardrobe from './pages/Wardrobe'
 
 function App() {
   // const date = new Date(Date.now());
   return (
     <>
-<<<<<<< HEAD
-     <HomePage />
-      
-=======
       <Navbar />
       {/* <GenerateOutfit /> */}
       <Wardrobe />
 
->>>>>>> 2c0948850b490aac93efeddf521e8484fe14e9a6
     </>
   )
 }

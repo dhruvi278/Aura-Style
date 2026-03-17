049f2807b4b919bbe8ecce65074d0274ec7f2309
Jayminsinh <jaymin.parmar@aliansoftware.com>
2026-03-17 09:51:36 +0530

    GenerateOutfit style Changes

--- src/pages/GenerateOutfit.jsx
+++ src/pages/GenerateOutfit.jsx
@@ -6,9 +6,9 @@ import AIResponce from '../components/generateOutfit/AIResponce'
 
 function GenerateOutfit() {
     return (
-        <main className='flex flex-col gap-5 px-4 sm:px-6 md:px-10 pt-6 sm:pt-8 md:pt-12 bg-[#E7E1CF]/50 lg:h-[91.2vh]'>
+        <main className='flex flex-col gap-5 px-4 sm:px-6 md:px-10 pt-6 sm:pt-8 md:pt-12 bg-[#E7E1CF]/50 min-h-[calc(100dvh-80px)]'>
             <TitleText title='Style Your Movement' description='Generate a trendy outfit from your wardrobe and elevate your everyday style.' />
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-10'>
                 <OutfitForm />
                 <OutfitCard />
                 <aside className='block md:hidden mb-10 px-2.5'>
